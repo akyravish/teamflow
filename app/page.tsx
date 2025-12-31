416b8a0d2dcdea2a6ae37e193fe0bbf853a2dd44
Akshay Ravish <akshay@antiersolutions.com>
2025-12-31 17:20:02 +0530

    Add hero section and header components with animations; update configuration and dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import { HeroHeader } from '@/components/header';
+import HeroSection from '@/components/hero-section';
+
 export default function Home() {
 	return (
 		<div>
-			<h1>Hello World</h1>
+			<HeroHeader />
+			<HeroSection />
 		</div>
 	);
 }
